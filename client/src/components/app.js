a6d9c7c1ff6ed42da661ec457818f27b1d0de4c7
Kaylea Britton <kbbritton217@gmail.com>
2017-09-19 09:41:45 -0400

    initial commit

---
+++ client/src/components/app.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import NavBarHeader from './Nav';
+
+	export default class App extends Component {
+  		render() {
+    			return (
+      			<div>
+	      			<NavBarHeader />
+      			</div>
+    			);
+  		}
+	}
